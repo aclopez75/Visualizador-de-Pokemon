9eda497f39177dc607f513052d19e74e3f53268e
Laia Clemente <laiacp2002@gmail.com>
2025-03-10 22:00:30 +0100

    añadir función info pokemon

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ async function fetchPokemon(pokemonName) {
         // alert("We haven't found any Pokemon with that name in our database.");
     }
 }
+
+// Función para mostrar la información del Pokémon.
+function displayPokemon(data) {
+    const pokemonInfo = document.getElementById('container-card');
+
+    pokemonInfo.innerHTML = `
+        <div id="pokemonData">
+            <img src="${data.sprites.front_default}" alt="${data.name}">
+            <img src="${data.sprites.back_default}" alt="${data.name}">
+        </div>
+        <h2>${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h2>
+        <p><strong>Peso:</strong> ${data.weight}</p>
+        <p><strong>Altura:</strong> ${data.height}</p>
+        <p><strong>Tipos:</strong> ${data.types.map(type => type.type.name).join(', ')}</p>
+        <p><strong>Estadísticas:</strong></p>
+        <ul>
+            ${data.stats.map(stat => 
+                <li><strong>${stat.stat.name}:</strong> ${stat.base_stat}</li>
+            ).join('')}
+        </ul>
+    `;
+}
